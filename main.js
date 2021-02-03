83446462e03fb4031b15eed10501999cc7786030
salmo jr <salmo.jr@gmail.com>
2021-02-03 18:26:26 -0300

    chore: implements the custom menu

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import 'babel-polyfill';
 
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
 
 let mainWindow;
 
@@ -13,9 +14,11 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
     }
   });
+
+  //mainWindow.removeMenu();
 
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
@@ -23,12 +26,61 @@ const createWindow = () => {
     slashes: true
   }));
 
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
 };
+
+const templateMenu = [
+  {
+    label: 'Início',
+    submenu: [
+      {
+        label: 'Apresentação',
+        click: () => {
+          mainWindow.webContents.executeJavaScript("location.assign('#');");
+        }
+      }
+    ]
+  },
+  {
+    label: 'Cadastro',
+    submenu: [
+      {
+        label: 'Banda',
+        click: function() {
+          shell.openExternal('https://www.electronjs.org/');
+        }
+      },
+      {label: 'Cantor'},
+    ]
+  },
+  {
+    label: 'Relatório',
+    submenu: [
+      {
+        label: 'Bandas'
+      },
+      {
+        label: 'Cantores', 
+        click: () => {
+          mainWindow.webContents.executeJavaScript("location.assign('#deliveryman');");
+        }
+      }
+    ]
+  },
+  {
+    label: 'Entre em Contato',
+    click: () => {
+      mainWindow.webContents.executeJavaScript("location.assign('#deliveryman');");
+    }
+  }
+];
+
+const menu = Menu.buildFromTemplate(templateMenu);
+Menu.setApplicationMenu(menu);
 
 app.on('ready', createWindow);
 
