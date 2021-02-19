1de21a5bf55e9d2ca0d75d747462f8b1c53e8958
salmo jr <salmo.jr@gmail.com>
2021-02-19 18:52:11 -0300

    chore: inserts notification system

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GlobalMenu from '../../components/GlobalMenu';
+import { remote } from 'electron';
+import path from 'path';
 
 const Home = () => {
-    return(
-        <>
-            <GlobalMenu />
-            <h1>Home</h1>
-        </>
-    );
+  const [title, setTitle] = useState();
+  const [msg, setMsg] = useState();
+
+  const showNotification = (title, msg) => {
+    const notification = {
+      title: title,
+      body: msg,
+      icon: path.join(__dirname, '../../assets/logo.png'),
+    }
+    new remote.Notification(notification).show();
+  }
+
+  return (
+    <>
+      <GlobalMenu />
+      <h1>Home</h1>
+      <form>
+        <div>
+          <label htmlFor='title'>Título: </label>
+          <input
+            id='title'
+            name='title'
+            type='text'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor='msg'>Mensagem: </label>
+          <input
+            id='msg'
+            name='msg'
+            type='text'
+            value={msg}
+            onChange={(e) => setMsg(e.target.value)}
+          />
+        </div>
+      </form>
+      <button type="button" onClick={() => showNotification(title, msg)}>Notificação</button>
+    </>
+  );
 }
 
 export default Home;
