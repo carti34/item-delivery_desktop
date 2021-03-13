7eb693f0b8cce9d6c1c9e0948a4f594d67c412eb
Salmo Marques <salmo.sjunior@sp.senac.br>
2021-03-12 21:22:04 -0300

    refactor: improves AssignOrder

--- src/pages/AssignOrder/index.js
+++ src/pages/AssignOrder/index.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import Board from '@lourenci/react-kanban';
+import api from '../../services/api';
 import GlobalMenu from '../../components/GlobalMenu';
 
-const board = {
+/* const board = {
     columns: [
         {
             id: 1,
@@ -66,11 +67,96 @@ const board = {
             cards: [],
         },
     ]
-}
+} */
 
 const AssignOrder = () => {
     const [newBoard, setNewBoard] = useState(null);
     const [printBoard, setPrintBoard] = useState(false);
+    const [deliverymen, setDeliverymen] = useState([]);
+    const [board, setBoard] = useState();
+
+    const loadingDeliverymen = useCallback(async () => {
+        try {
+            const response = await api.get('/users');
+
+            if (response.data) {
+                setDeliverymen(response.data);
+            } else {
+                alert("Não foi encontrado nenhum entregador.");
+            }
+        } catch (error) {
+            alert(`Ocorreu uma falha ao retornar a lista de entregadores. ${error}`);
+        }
+    }, []);
+
+    const createBoard = useCallback(() => {
+        const board = {
+            columns: [
+                {
+                    id: 1,
+                    title: '# Pedidos #',
+                    cards: [
+                        {
+                            id: 1,
+                            title: 'pedido #1',
+                            description: 'Vila Prado [ 19:00 ]',
+                        },
+                        {
+                            id: 2,
+                            title: 'pedido #2',
+                            description: 'Pacaembu [ 20:15 ]',
+                        },
+                        {
+                            id: 3,
+                            title: 'pedido #3',
+                            description: 'Vila São José [ 20:40 ]',
+                        },
+                        {
+                            id: 4,
+                            title: 'pedido #4',
+                            description: 'Aracy [ 21:00 ]',
+                        },
+                        {
+                            id: 5,
+                            title: 'pedido #5',
+                            description: 'Boa Vista [ 21:10 ]',
+                        },
+                        {
+                            id: 6,
+                            title: 'pedido #6',
+                            description: 'Antenor Garcia [ 21:23 ]',
+                        },
+                        {
+                            id: 7,
+                            title: 'pedido #7',
+                            description: 'São Rafael [ 21:30 ]',
+                        },
+                        {
+                            id: 8,
+                            title: 'pedido #8',
+                            description: 'Douradinho [ 22:00 ]',
+                        },
+                    ],
+                },
+            ]
+        } 
+        const de = deliverymen.map(d => {
+            return (
+                {
+                    id: d.id,
+                    title: d.login,
+                    cards: [],
+                }
+            );
+        });
+        board.columns = [...board.columns, ...de];
+        setBoard(board);
+    }, [deliverymen]);
+
+    useEffect(() => {
+        loadingDeliverymen();
+        createBoard();
+    }, [loadingDeliverymen, createBoard]);
 
     return (
         <>
