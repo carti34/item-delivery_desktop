c5bd704a771b46de99516f5d7751d65a0d346f23
salmo jr <salmo.jr@gmail.com>
2021-03-16 16:55:26 -0300

    chore: dynamically creates deliverymen column

--- src/pages/AssignOrder/index.js
+++ src/pages/AssignOrder/index.js
@@ -3,72 +3,6 @@ import Board from '@lourenci/react-kanban';
 import api from '../../services/api';
 import GlobalMenu from '../../components/GlobalMenu';
 
-/* const board = {
-    columns: [
-        {
-            id: 1,
-            title: '# Pedidos #',
-            cards: [
-                {
-                    id: 1,
-                    title: 'pedido #1',
-                    description: 'Vila Prado [ 19:00 ]',
-                },
-                {
-                    id: 2,
-                    title: 'pedido #2',
-                    description: 'Pacaembu [ 20:15 ]',
-                },
-                {
-                    id: 3,
-                    title: 'pedido #3',
-                    description: 'Vila São José [ 20:40 ]',
-                },
-                {
-                    id: 4,
-                    title: 'pedido #4',
-                    description: 'Aracy [ 21:00 ]',
-                },
-                {
-                    id: 5,
-                    title: 'pedido #5',
-                    description: 'Boa Vista [ 21:10 ]',
-                },
-                {
-                    id: 6,
-                    title: 'pedido #6',
-                    description: 'Antenor Garcia [ 21:23 ]',
-                },
-                {
-                    id: 7,
-                    title: 'pedido #7',
-                    description: 'São Rafael [ 21:30 ]',
-                },
-                {
-                    id: 8,
-                    title: 'pedido #8',
-                    description: 'Douradinho [ 22:00 ]',
-                },
-            ],
-        },
-        {
-            id: 2,
-            title: 'Mario',
-            cards: [],
-        },
-        {
-            id: 3,
-            title: 'João',
-            cards: [],
-        },
-        {
-            id: 4,
-            title: 'Zé',
-            cards: [],
-        },
-    ]
-} */
-
 const AssignOrder = () => {
     const [newBoard, setNewBoard] = useState(null);
     const [printBoard, setPrintBoard] = useState(false);
@@ -77,20 +11,21 @@ const AssignOrder = () => {
 
     const loadingDeliverymen = useCallback(async () => {
         try {
-            const response = await api.get('/users');
+            const response = await api.get('/users?per_page=4');
 
             if (response.data) {
                 setDeliverymen(response.data);
             } else {
                 alert("Não foi encontrado nenhum entregador.");
             }
+            createBoard(response.data);
         } catch (error) {
             alert(`Ocorreu uma falha ao retornar a lista de entregadores. ${error}`);
         }
     }, []);
 
-    const createBoard = useCallback(() => {
-        const board = {
+    const createBoard = useCallback((deliverymen) => {
+        const boardUpdate = {
             columns: [
                 {
                     id: 1,
@@ -139,36 +74,37 @@ const AssignOrder = () => {
                     ],
                 },
             ]
-        } 
-        const de = deliverymen.map(d => {
+        };
+        const de = deliverymen.map((d, index) => {
             return (
                 {
-                    id: d.id,
+                    id: index + 2,
                     title: d.login,
                     cards: [],
                 }
             );
         });
-        board.columns = [...board.columns, ...de];
-        setBoard(board);
+        boardUpdate.columns = [...boardUpdate.columns, ...de];
+        setBoard(boardUpdate);
     }, [deliverymen]);
 
     useEffect(() => {
         loadingDeliverymen();
-        createBoard();
-    }, [loadingDeliverymen, createBoard]);
+    }, []);
 
     return (
         <>
             <GlobalMenu />
             <h1>Encaminhamento de Pedidos</h1>
-            <Board
-                initialBoard={board}
-                onCardDragEnd={(board) => {
-                    setPrintBoard(false);
-                    setNewBoard(board);
-                }}
-            />
+            {board ?
+                <Board
+                    initialBoard={board}
+                    onCardDragEnd={(board) => {
+                        setPrintBoard(false);
+                        setNewBoard(board);
+                    }}
+                /> : null}
+
             <button
                 type="button"
                 onClick={newBoard ?
